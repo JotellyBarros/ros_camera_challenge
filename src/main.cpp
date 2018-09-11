cec3a416c7223eac36f308234065e4a74c529362
Jotelly Barros <jotelly@gmail.com>
2018-09-11 13:56:56 -0300

    Add launch file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <ros_camera_app.hpp>
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "dynamic_reconfigure");
+  ros::init(argc, argv, "dynamic_cam");
 
   RosCamera ros_camera_obj;
 
